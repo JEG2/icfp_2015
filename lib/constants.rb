b2ed733b4ec49a7ff318c6ed34a9f0733c896b69
Calvyn82 <claytonflesher@gmail.com>
2015-08-08 18:43:31 -0500

    necronomicon

--- lib/constants.rb
+++ lib/constants.rb
@@ -1,3 +1,3 @@
-POWER_WORDS = [ "ia! ia! ", "yogsothoth", "cthulhu fhtagn!", "yuggoth", "blue hades", "r'lyeh", "ei!"]
+POWER_WORDS = [ "ia! ia! ", "yogsothoth", "cthulhu fhtagn!", "yuggoth", "blue hades", "r'lyeh", "ei!", "necronomicon"]
 
 POTENTIALS  = [ "Ph'nglui mglw'nafh Cthulhu R'lyeh wgah'nagl fhtagn.", "Necronomicon", "H.P. Lovecraft", "HP Lovecraft", "Tekeli-li! Tekeli-li!", "Al Azif", "The Old Ones", "Hei! Hei!", "Malitia vetus—malitia vetus", "Arkham", "Miskatonic University", "DIES MIES JESCHET BOENE DOESEF DOUVEMA ENITEMAUS", "PER ADONAI ELOIM, ADONAI JEHOVA, ADONAI SABAOTH, METRATON"] 
