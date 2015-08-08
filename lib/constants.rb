f2baa35ea106d749a254b6fc12c415f06f895095
Calvyn82 <claytonflesher@gmail.com>
2015-08-07 23:01:53 -0500

    Yogsothoth is a constant

--- lib/constants.rb
+++ lib/constants.rb
@@ -1,4 +1,4 @@
-POWER_WORDS = [ "ei!",
+POWER_WORDS = [ "ei!", "yogsothoth",
                 "in the house at r'lyeh dead cthulhu waits dreaming.",
                 "Ph'nglui mglw'nafh Cthulhu R'lyeh wgah'nagl fhtagn."
               ]
