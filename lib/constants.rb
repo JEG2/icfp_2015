ec43e25d507a559ff3eb87ea876aa37cb07b9c8f
Calvyn82 <claytonflesher@gmail.com>
2015-08-08 18:30:02 -0500

    cthulhu fhtagn!

--- lib/constants.rb
+++ lib/constants.rb
@@ -1,5 +1,3 @@
-POWER_WORDS = [ "Ph'nglui mglw'nafh Cthulhu R'lyeh wgah'nagl fhtagn.", 
-                "ia! ia! ", "yogsothoth", "yuggoth", "blue hades", "r'lyeh", "ei!"
-              ]
+POWER_WORDS = [ "ia! ia! ", "yogsothoth", "cthulhu fhtagn!", "yuggoth", "blue hades", "r'lyeh", "ei!"]
 
-POTENTIALS  = [ "Cthulhu fhtagn!", "Ph'nglui mglw'nafh Cthulhu R'lyeh wgah'nagl fhtagn.", "Necronomicon", "H.P. Lovecraft", "HP Lovecraft", "Tekeli-li! Tekeli-li!", "Al Azif", "The Old Ones", "Hei! Hei!", "Malitia vetus—malitia vetus", "Arkham", "Miskatonic University", "DIES MIES JESCHET BOENE DOESEF DOUVEMA ENITEMAUS", "PER ADONAI ELOIM, ADONAI JEHOVA, ADONAI SABAOTH, METRATON"] 
+POTENTIALS  = [ "Ph'nglui mglw'nafh Cthulhu R'lyeh wgah'nagl fhtagn.", "Necronomicon", "H.P. Lovecraft", "HP Lovecraft", "Tekeli-li! Tekeli-li!", "Al Azif", "The Old Ones", "Hei! Hei!", "Malitia vetus—malitia vetus", "Arkham", "Miskatonic University", "DIES MIES JESCHET BOENE DOESEF DOUVEMA ENITEMAUS", "PER ADONAI ELOIM, ADONAI JEHOVA, ADONAI SABAOTH, METRATON"] 
