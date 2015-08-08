1adde8171a12389acf878a46627fc754332910db
Calvyn82 <claytonflesher@gmail.com>
2015-08-08 08:58:46 -0500

    fixed spelling of r'lyeh

--- lib/constants.rb
+++ lib/constants.rb
@@ -1,4 +1,4 @@
-POWER_WORDS = [ "ei!", "yogsothoth",
+POWER_WORDS = [ "ei!", "yogsothoth", "cthulhu", "r'lyeh",
                 "in his house at r'lyeh dead cthulhu waits dreaming.",
                 "Ph'nglui mglw'nafh Cthulhu R'lyeh wgah'nagl fhtagn."
               ]
