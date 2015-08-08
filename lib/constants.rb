f04323f384759cd768b99a3f31c7cde7021f57c4
Calvyn82 <claytonflesher@gmail.com>
2015-08-08 18:04:55 -0500

    Created a POTENTIALS constant

--- lib/constants.rb
+++ lib/constants.rb
@@ -1,3 +1,5 @@
 POWER_WORDS = [ "Ph'nglui mglw'nafh Cthulhu R'lyeh wgah'nagl fhtagn.", 
-                "ia! ia! cthulhu fhtagn!", "yogsothoth", "yuggoth", "blue hades", "r'lyeh", "ei!"
+                "ia! ia! ", "yogsothoth", "yuggoth", "blue hades", "r'lyeh", "ei!"
               ]
+
+POTENTIALS  = [ "Cthulhu fhtagn!", "Ph'nglui mglw'nafh Cthulhu R'lyeh wgah'nagl fhtagn.", "Necronomicon", "H.P. Lovecraft", "HP Lovecraft", "Tekeli-li! Tekeli-li!", "Al Azif", "The Old Ones", "Hei! Hei!", "Malitia vetus—malitia vetus", "Arkham", "Miskatonic University", "DIES MIES JESCHET BOENE DOESEF DOUVEMA ENITEMAUS", "PER ADONAI ELOIM, ADONAI JEHOVA, ADONAI SABAOTH, METRATON"] 
