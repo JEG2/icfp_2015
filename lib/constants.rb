7a154b8646906ae9cd6f39b40d0f3309cbcc62b0
Calvyn82 <claytonflesher@gmail.com>
2015-08-09 10:36:46 -0500

    new power phrase

--- lib/constants.rb
+++ lib/constants.rb
@@ -1,5 +1,5 @@
-POWER_WORDS = ["planet 10", "ph'nglui mglw'nafh cthulhu r'lyeh wgah'nagl fhtagn.", "ia! ia! ", "yogsothoth", "cthulhu fhtagn!", "yuggoth", "blue hades", "r'lyeh", "ei!", "necronomicon"]
+POWER_WORDS = ["john bigboote", "planet 10", "ph'nglui mglw'nafh cthulhu r'lyeh wgah'nagl fhtagn.", "ia! ia! ", "yogsothoth", "cthulhu fhtagn!", "yuggoth", "blue hades", "r'lyeh", "ei!", "necronomicon"]
 
-POTENTIALS  = ["The Old Ones"] 
+POTENTIALS  = ["The Old Ones", "seattle"] 
 
 WRONGS = ["AS2H2", "hp lovecraft", "tekeli li! tekeli li!", "al azif", "arkham", "miskatonic", "Malitia vetus—malitia vetus", "DIES MIES JESCHET BOENE DOESEF DOUVEMA ENITEMAUS", "PER ADONAI ELOIM, ADONAI JEHOVA, ADONAI SABAOTH, METRATON", "galois inc", "buckaroo banzai"] 
