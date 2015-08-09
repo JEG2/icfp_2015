4a13616498ca752c104f59608ad23d4afe5d64b0
Calvyn82 <claytonflesher@gmail.com>
2015-08-09 13:30:53 -0500

    current list

--- lib/constants.rb
+++ lib/constants.rb
@@ -2,4 +2,4 @@ POWER_WORDS = ["yoyodyne", "john bigboote", "planet 10", "ph'nglui mglw'nafh cth
 
 POTENTIALS  = ["ray kurzweil"] 
 
-WRONGS = ["AS2H2", "hp lovecraft", "tekeli li! tekeli li!", "al azif", "arkham", "miskatonic", "Malitia vetus—malitia vetus", "DIES MIES JESCHET BOENE DOESEF DOUVEMA ENITEMAUS", "PER ADONAI ELOIM, ADONAI JEHOVA, ADONAI SABAOTH, METRATON", "galois inc", "buckaroo banzai", "pluto", "seattle", "laugh while you can monkey boy!", "The Old Ones", "director aleister", "blue blaze irregular", "computational archeology", "hex", "tetris", "other government agency", "oga"]
+WRONGS = ["AS2H2", "hp lovecraft", "tekeli li! tekeli li!", "al azif", "arkham", "miskatonic", "Malitia vetus—malitia vetus", "DIES MIES JESCHET BOENE DOESEF DOUVEMA ENITEMAUS", "PER ADONAI ELOIM, ADONAI JEHOVA, ADONAI SABAOTH, METRATON", "galois inc", "buckaroo banzai", "pluto", "seattle", "laugh while you can monkey boy!", "The Old Ones", "director aleister", "blue blaze irregular", "computational archeology", "hex", "tetris", "other government agency", "oga", "the singularity", "NASA", "quantum computer"]
