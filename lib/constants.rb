bb3abe0e86e3e8c3d2352d4f56dd5eba25b17628
Calvyn82 <claytonflesher@gmail.com>
2015-08-08 21:31:35 -0500

    current list

--- lib/constants.rb
+++ lib/constants.rb
@@ -1,5 +1,5 @@
-POWER_WORDS = ["ia! ia! ", "yogsothoth", "cthulhu fhtagn!", "yuggoth", "blue hades", "r'lyeh", "ei!", "necronomicon"]
+POWER_WORDS = ["ph'nglui mglw'nafh cthulhu r'lyeh wgah'nagl fhtagn.", "ia! ia! ", "yogsothoth", "cthulhu fhtagn!", "yuggoth", "blue hades", "r'lyeh", "ei!", "necronomicon"]
 
-POTENTIALS  = [ "Ph'nglui mglw'nafh Cthulhu R'lyeh wgah'nagl fhtagn.", "The Old Ones", "Malitia vetus—malitia vetus", "DIES MIES JESCHET BOENE DOESEF DOUVEMA ENITEMAUS", "PER ADONAI ELOIM, ADONAI JEHOVA, ADONAI SABAOTH, METRATON"] 
+POTENTIALS  = ["The Old Ones", "Malitia vetus—malitia vetus", "DIES MIES JESCHET BOENE DOESEF DOUVEMA ENITEMAUS", "PER ADONAI ELOIM, ADONAI JEHOVA, ADONAI SABAOTH, METRATON"] 
 
 WRONGS = ["AS2H2", "hp lovecraft", "tekeli li! tekeli li!", "al azif", "arkham", "miskatonic"]
