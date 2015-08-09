adcbedefc7bcc2d8b4f780797c22f162399edcaa
Calvyn82 <claytonflesher@gmail.com>
2015-08-08 19:32:03 -0500

    move tekeli li! to wrong

--- lib/constants.rb
+++ lib/constants.rb
@@ -1,5 +1,5 @@
 POWER_WORDS = ["ia! ia! ", "yogsothoth", "cthulhu fhtagn!", "yuggoth", "blue hades", "r'lyeh", "ei!", "necronomicon"]
 
-POTENTIALS  = [ "Ph'nglui mglw'nafh Cthulhu R'lyeh wgah'nagl fhtagn.", "Tekeli-li! Tekeli-li!", "Al Azif", "The Old Ones", "Hei! Hei!", "Malitia vetus—malitia vetus", "Arkham", "Miskatonic University", "DIES MIES JESCHET BOENE DOESEF DOUVEMA ENITEMAUS", "PER ADONAI ELOIM, ADONAI JEHOVA, ADONAI SABAOTH, METRATON"] 
+POTENTIALS  = [ "Ph'nglui mglw'nafh Cthulhu R'lyeh wgah'nagl fhtagn.", "Al Azif", "The Old Ones", "Hei! Hei!", "Malitia vetus—malitia vetus", "Arkham", "Miskatonic University", "DIES MIES JESCHET BOENE DOESEF DOUVEMA ENITEMAUS", "PER ADONAI ELOIM, ADONAI JEHOVA, ADONAI SABAOTH, METRATON"] 
 
 WRONGS = ["AS2H2", "hp lovecraft", "tekeli li! tekeli li!"]
